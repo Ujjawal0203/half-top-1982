6da4af367459ee023de6bcd9898e395d62f0c4fd
Vedant <vedant.sddeshmukh@gmail.com>
2022-09-29 20:21:04 +0530

    added day3 work

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// slider animation
 const swiper = new Swiper('.swiper', {
     autoplay: {
         delay: 3000,
@@ -18,3 +19,129 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   });
+
+
+
+
+  // ********************************************
+
+  // filtered products
+  let makeup = [
+    {
+      image:
+        "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/8/5/85ee43en_3401575390447-01.jpg",
+      title: "bioderma sensibio h2o makeup removing micellar cleansing",
+      weight: "(250ml)",
+      quantity: 1,
+      id: "BBJ0003890",
+      strikedOffPrice: "990.00",
+      price: "910.00",
+      discount: "8% Off",
+      brand:'Bioderma',
+    },
+    {
+      image:
+        "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/8/c/8c9c39226479_s1-8901030897429.jpg",
+      title: "indulekha bringha hair strengthening lepam",
+      weight: "(200ml)",
+      quantity: 1,
+      id: "BBJ0003875",
+      strikedOffPrice: "648.00",
+      price: "603.00",
+      discount: "7% off",
+      brand: 'Indulekha',
+    },
+    {
+      image:
+        "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/3/c/3cabc5a8901030805080_2.jpg",
+      title: "sunsilk almond & honey shampoo",
+      weight: "(370ml)",
+      quantity: 1,
+      id: "BBJ0003876",
+      strikedOffPrice: "345.00",
+      price: "315.00",
+      discount: "9% Off",
+      brand: 'Sunsilk',
+    },
+    {
+      image:
+        "https://www.beautybebo.com/pub/media/catalog/product/cache/37253e89591b79b38c00254331932999/8/e/8e0ca8ctrese00000040_1.jpg",
+      title: "TRESemme Pro Pure Damage Recovery Shampoo",
+      weight: "(390ml)",
+      quantity: 1,
+      id: "BBJ0003877",
+      strikedOffPrice: "500.00",
+      price: "465.00",
+      discount: "7% Off",
+      brand: 'Tresemme',
+    },
+  ];
+  
+  var arr = JSON.parse(localStorage.getItem("icon")) || [];
+  append(makeup);
+  
+  function append(makeup) {
+    makeup.map(function (elem) {
+      // console.log(elem)
+      var div = document.createElement("div");
+  
+      var image = document.createElement("img");
+      image.src = elem.image;
+      image.addEventListener("click",function(){
+        aariftit(elem) 
+      })
+  
+      var title = document.createElement("p");
+      title.innerText = elem.title;
+      title.addEventListener("click",function(){
+        aariftit(elem) 
+      })
+  
+      let rating = document.createElement("div");
+      rating.setAttribute("id", "rating");
+      rating.innerHTML = `
+      <span>(1)</span>`;
+  
+      var div2 = document.createElement("div");
+      div2.setAttribute("id", "price_tag");
+  
+      var sdprice = document.createElement("h4");
+      sdprice.setAttribute("id", "cut_price_tag");
+      sdprice.innerText = `₹${elem.strikedOffPrice}`;
+  
+      var price = document.createElement("h4");
+      price.setAttribute("id", "price_color");
+      price.innerText = `₹${elem.price}`;
+  
+      var dis = document.createElement("h4");
+      dis.setAttribute("id", "dis_price");
+      dis.innerText = elem.discount;
+  
+      var div3 = document.createElement("div");
+      div3.setAttribute("id", "cart_div");
+  
+      var btn = document.createElement("button");
+      btn.innerHTML = '<img src="https://delightful-duckanoo-17fff8.netlify.app/cart-38-24.png">';
+      btn.addEventListener("click", function () {
+        addel(elem);
+      });
+  
+      let heart = document.createElement("span");
+      heart.innerHTML = "🤍";
+      heart.setAttribute("id", "whish_list");
+  
+      let spanBtn = document.createElement("span");
+      spanBtn.innerText = "Add To Cart";
+      btn.append(spanBtn);
+  
+      div2.append(sdprice, price, dis);
+      div3.append(btn, heart);
+      div.append(image, title, rating, div2, div3);
+  
+      document.getElementById("filter-products").append(div);
+    });
+  }
+
+  // *****************************************
+
+  
