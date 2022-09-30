ed31325465a397259e44f635ce2efc18111f378c
Rishu jha <jharishu796@gmail>
2022-09-30 20:11:22 +0530

    ayurveda page and updates

--- script_append/append.js
+++ script_append/append.js
@@ -12,27 +12,35 @@ const append = (data,container) =>{
         title.innerText = element.title;
         title.style.height = "20px";
         let price = document.createElement("h4");
-        price.innerText = element.price;
+        price.innerText = "₹";
+        let span = document.createElement("span");
+        span.innerText = element.price;
+        span.id = "num";
+        price.append(span);
 
         //making buttons---------------------;
-        //btn_div_main is main div we will append add to cart and image in this div;
+        //btn_div_main is main div we will append add to cart and image/heart in this div;
         let btn_div_main = document.createElement("div");
         btn_div_main.id = "buttons";
+        btn_div_main.style.marginBottom ="20px";
 
         let addToCart = document.createElement("button");
         addToCart.id = 'cart';
-        addToCart.innerText = "Add To Cart";
+        addToCart.innerText = "🛒Add To Cart";
 
         //div to append heart image
         let like_div = document.createElement("div");
         like_div.id = "like";
+        like_div.style.backgroundColor = " rgb(70, 69, 69)";
+        like_div.style.display = "flex";
+        like_div.style.alignItems = "center";
 
         //making image tag for heart image;
-        let like_img = document.createElement("img");
-        like_img.src = "https://cdn-icons-png.flaticon.com/512/3237/3237429.png";
+        let like_img = document.createElement("div");
+        like_img.innerHTML = "🤍";
         like_div.append(like_img);
 
-
+        
         //appending addToCart button and like_div to btn_div_main;
         btn_div_main.append(addToCart,like_div);
         
