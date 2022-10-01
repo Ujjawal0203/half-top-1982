892129608ebfed9bf6eb80dabf491c04ae1559a1
Ujjawal Kumar <ujjawal0203.jk@gmail.com>
2022-10-02 00:54:24 +0530

    ansu

---
+++ components/navbar.js
@@ -0,0 +1,361 @@
+function navbar() {
+    return `
+    <div id="nav-top">
+    <div><a href="index.html"><img id="logo"
+                src="https://www.beautybebo.com/pub/media/logo/default/beautybebo_1.png" alt=""></a></div>
+    <div id="search">
+        <div id="Card">
+            <select>
+                <option value="">All categories</option>
+                <option value="Makeup">--Makeup</option>
+                <option value="">----Face</option>
+                <option value="">------Foundation</option>
+                <option value="">------Blush</option>
+                <option value="">------Highlighters</option>
+                <option value="">------Concealer</option>
+                <option value="">------Face Primer</option>
+                <option value="">------Contour</option>
+                <option value="">------Bronzer</option>
+                <option value="">------Eye</option>
+                <option value="">------Eyeliner</option>
+                <option value="">------Mascara</option>
+                <option value="">------Kajal</option>
+                <option value="">------Eye Primer</option>
+                <option value="">------Eye Kit</option>
+                <option value="">------Under Eye Concealer</option>
+            </select>
+        </div>
+        <div>
+            <input type="search" placeholder="Enter your search..">
+        </div>
+        <div>
+            <img src="https://icon-library.com/images/white-search-icon-transparent-background/white-search-icon-transparent-background-1.jpg"
+                alt="">
+        </div>
+    </div>
+    <div id="login_btn">
+        <div>
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/004/916/039/small/heart-icon-perfect-love-symbol-valentine-s-day-sign-emblem-isolated-on-white-background-with-shadow-flat-style-for-graphic-and-web-design-logo-free-vector.jpg"
+                alt="">
+        </div>
+        <div>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/2048px-OOjs_UI_icon_userAvatar.svg.png"
+                alt="">
+        </div>
+        <div id="aa">
+            <a href="login.html">Login/</a><a href="register.html">Register</a>
+        </div>
+    </div>
+</div>
+<!-- navbar part2 -->
+<div id="navbar">
+    <div class="main">
+        <div class="nav">
+            <ul>
+                <li><a href=""> MAKEUP</a>
+                    <div class="megamenu">
+                        <div class="content">
+                            <h4>FACE</h4>
+                            <p>BB Cream</p>
+                            <p>Blush</p>
+                            <p>Bronze</p>
+                            <p>CC Cream</p>
+                            <p>Contour</p>
+                            <p>Concealer</p>
+                            <p>compact & powder</p>
+                            <p>Face Primer</p>
+                            <p>Foundation</p>
+                            <p>Highlighters</p>
+                            <p>Loose Powder</p>
+                            <p>Makeup kits</p>
+                            <p>Makeup Remover</p>
+                            <p>Setting Spray</p>
+                        </div>
+                        <div class="content">
+                            <h4>EYE</h4>
+                            <p>Contact Lenses</p>
+                            <p>Eye Kit</p>
+                            <p>Eyeliner</p>
+                            <p>Eye Shadow</p>
+                            <p>Eye Primer</p>
+                            <p>Eye-Palettes</p>
+                            <p>Eye Makeup Remover</p>
+                            <p>Eye Brow Enhance</p>
+                            <p>False Eyelases</p>
+                            <p>Kajal</p>
+                            <p>Mascara </p>
+                            <p>Under Eye Concealer</p>
+                        </div>
+                        <div class="content">
+                            <h4>LIPS</h4>
+                            <p>Lipstik</p>
+                            <p>Liquid Lipstik</p>
+                            <p>Lip Liner</p>
+                            <p>Lip Gloss</p>
+                            <p>Lip Balm</p>
+                            <p>Lip Crayon</p>
+                            <p>Lip Stain</p>
+                            <p>Lip Plumper</p>
+                        </div>
+                        <div class="content">
+                            <h4>NAILS</h4>
+                            <p>MAnicure & Pedicure Kits</p>
+                            <p>Nail Polish</p>
+                            <p>Nail Care</p>
+                            <p>Nail Polish Sets</p>
+                            <p>Nail Art Kits</p>
+                            <p>Nail Polish Remover</p>
+                        </div>
+                        <div class="content">
+                            <h4>TOOLS BRUSHES</h4>
+                            <p>Blush Brush</p>
+                            <p>Eyelash Curlers</p>
+                            <p>Eye Brush</p>
+                            <p>Face Brush</p>
+                            <p>Lip Brush</p>
+                            <p>Makeup Pouches</p>
+                            <p>Mirrors</p>
+                            <p>Sponges & Applicators</p>
+                            <p>Sharpeners</p>
+                            <p>Tweezers</p>
+                        </div>
+                    </div>
+                </li>
+                <li><a href="">SKIN</a>
+                    <div class="megamenu">
+                        <div class="content">
+                            <h4>EYE CARE</h4>
+                            <p>Dark Circle</p>
+                            <p>Eye Contour</p>
+                            <p>Eye Cream</p>
+                            <p>Eye Masks</p>
+                            <p>Eye Serums</p>
+                            <p>Puffiness</p>
+                            <p>Under Eye Creams</p>
+                            <p>Under Eye Wrinkles</p>
+                        </div>
+                        <div class="content">
+                            <h4>FACE CARE</h4>
+                            <p>Anti-Ageing Creams</p>
+                            <p>Bleach Cream</p>
+                            <p>Face Wash</p>
+                            <p>Facial Wipes</p>
+                            <p>Face Oil</p>
+                            <p>Face Cleansers</p>
+                            <p>Facial Kits</p>
+                            <p>Face Tools </p>
+                            <p>Moisturizer </p>
+                            <p>Mask & Peels </p>
+                            <p>Serum </p>
+                            <p> Toner & Astringents </p>
+                        </div>
+                        <div class="content">
+                            <h4>Body CARE</h4>
+                            <p>All Cream</p>
+                            <p>Body Moisturizer</p>
+                            <p>Body Toners</p>
+                            <p>Body Sun Care</p>
+                            <p>Brightening Lotion</p>
+                            <p>Dark Circles & Wrinkles</p>
+                            <p>Day Cream</p>
+                            <p>Foot Cream</p>
+                            <p>Hair Remover Cream</p>
+                            <p>Hand Creams</p>
+                            <p>Hands & Feet</p>
+                            <p>Kits & Combos</p>
+                            <p>Night Cream</p>
+                            <p>Neck Cream</p>
+                        </div>
+                        <div class="img">
+                            <img src="https://www.beautybebo.com/pub/media/mega-menu/SKIN_cATA.jpg" alt="">
+                        </div>
+                    </div>
+                </li>
+                <li><a href="#">HAIR</a>
+                    <div class="megamenu">
+                        <div class="content">
+                            <h4>HAIR CARE</h4>
+                            <p>Color Protection</p>
+                            <p>Dandruff</p>
+                            <p>Dry Shampoo</p>
+                            <p>Gels & Waxes</p>
+                            <p>Hair Spray</p>
+                            <p>Hair Color/Dye</p>
+                            <p>Hair Creams & Masks</p>
+                            <p>Hair Styling</p>
+                            <p>Hairfall & Thinning</p>
+                            <p>Straighteners</p>
+                        </div>
+                        <div class="content">
+                            <h4>HAIR LOSS</h4>
+                            <p>Conditioner</p>
+                            <p>Hair Oil</p>
+                            <p>Hair Serum</p>
+                            <p>Hair Growth Solution</p>
+                            <p>Shampoo</p>
+                        </div>
+                        <div class="img">
+                            <img src="https://www.beautybebo.com/pub/media/mega-menu/hair_cata.jpg" alt="">
+                        </div>
+                    </div>
+                </li>
+                <li><a href="#">PERSONAL CARE</a>
+                    <div class="megamenu">
+                        <div class="content">
+                            <h4>BATH & Body</h4>
+                            <p>Body Cleaners</p>
+                            <p>Body Massage Oil</p>
+                            <p>Body Wash</p>
+                            <p>Creams</p>
+                            <p>Essential Oils</p>
+                            <p>Foot Creams</p>
+                            <p>Scrubs & Exfoliants</p>
+                            <p>Ubtan & Face Packs</p>
+                        </div>
+                        <div class="content">
+                            <h4>BATHING ACCESSORIES</h4>
+                            <p>Bath Brushes</p>
+                            <p>Loofahs</p>
+                            <p>Shower Caps</p>
+                            <p>Sponges</p>
+                        </div>
+                        <div class="content">
+                            <h4>BATH & SHOWER</h4>
+                            <p>Body Soaps</p>
+                            <p>Shower Gel</p>
+                        </div>
+                        <div class="img">
+                            <img src="https://www.beautybebo.com/pub/media/mega-menu/personal_cata.jpg" alt="">
+                        </div>
+                    </div>
+                </li>
+                <li><a href="#">MOM & BABY CARE</a>
+                    <div class="megamenu">
+                        <div class="content">
+                            <h4>BABY CARE</h4>
+                            <p>Bath Time</p>
+                            <p>Diapers</p>
+                            <p>Lotions & Creams</p>
+                            <p>Oils</p>
+                            <p>Powder</p>
+                            <p>Shampoo</p>
+                            <p>Soaps</p>
+                            <p>Sterilizer</p>
+                            <p>Rash Cream</p>
+                            <p>Wipes</p>
+                        </div>
+                        <div class="content">
+                            <h4>MOM CARE</h4>
+                            <p>Lotions & Creams</p>
+                            <p>Personal Care</p>
+                        </div>
+                        <div class="img">
+                            <img src="https://www.beautybebo.com/pub/media/mega-menu/mom_baby_cata.jpg" alt="">
+                        </div>
+                    </div>
+                </li>
+                <li><a href="#">FRAGRANCE</a>
+                    <div class="frag">
+                        <div class="nance">
+                            <p>Perfumes</p>
+                            <p>Deodorants roll ons</p>
+                            <p>Body Mist/Spray</p>
+                        </div>
+                    </div>
+                </li>
+                <li><a href="#">AYURVEDA</a>
+                    <div class="megamenu">
+                        <div class="content">
+                            <h4>NATURAL SKIN CARE</h4>
+                            <p>Body Wash</p>
+                            <p>Body Lotion</p>
+                            <p>Cleaners</p>
+                            <p>Cream</p>
+                            <p>Eye Care</p>
+                            <p>Face Wash</p>
+                            <p>Gel</p>
+                            <p>Lip Care</p>
+                            <p>Masks</p>
+                            <p>Pack</p>
+                            <p>Scrub</p>
+                            <p>Sun Protection</p>
+                        </div>
+                        <div class="content">
+                            <h4>NATURAL HAIR CARE</h4>
+                            <p>Conditioner</p>
+                            <p>Hair Oils</p>
+                            <p>Hair Serum</p>
+                            <p>Hair Gel</p>
+                            <p>Shampoo</p>
+                        </div>
+                        <div class="img">
+                            <img src="https://www.beautybebo.com/pub/media/mega-menu/ayurveda_cata.jpg" alt="">
+                        </div>
+                    </div>
+                </li>
+                <li><a href="#">BRANDS</a>
+                    <div class="megamenu1">
+                        <div class="megam">
+                            <div class="top">
+                                <h4>Top Brands</h4>
+                            </div>
+                            <div><button id="btn" >View all</button></div>
+                        </div><br>
+                        <div class="brand">
+                            <div>
+                                <p>LAKME</p>
+                            </div>
+                            <div>
+                                <p>L-OREAL-PARIS</p>
+                            </div>
+                            <div>
+                                <p>JOY</p>
+                            </div>
+                            <div>
+                                <p>LOTUS HERBALS</p>
+                            </div>
+                            <div>
+                                <p>BIOTIQUE</p>
+                            </div>
+                            <div>
+                                <p>VLCC</p>
+                            </div>
+                            <div>
+                                <p>VEGA</p>
+                            </div>
+                            <div>
+                                <p>MAYBELLINE</p>
+                            </div>
+                            <div>
+                                <p>MAMAEARTH</p>
+                            </div>
+                            <div>
+                                <p>CHICCO</p>
+                            </div>
+                            <div>
+                                <p>NEUTROGENA</p>
+                            </div>
+                            <div>
+                                <p>AROMA MAGIC</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="cart">
+            <div class="cartp">
+                <img src="https://delightful-duckanoo-17fff8.netlify.app/cart-38-24.png" alt="">
+                <div class="c"><span>My Cart - </span></div>
+                <a href="cart.html">
+                    <span id="mycart"> </span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+    
+    
+    `
+}
+export default navbar;
